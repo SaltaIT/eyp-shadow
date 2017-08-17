c2cc8936b69e5021bddc414e160c43d536613ba1
Jordi Prats <jprats@systemadmin.es>
2017-08-17 10:27:19 +0200

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class shadow(
-              $manage_package        = true,
-              $package_ensure        = 'installed',
+              $manage_package = true,
+              $package_ensure = 'installed',
             ) inherits shadow::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
