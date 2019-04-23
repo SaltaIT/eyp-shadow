d96a92e64147e17ca854e3ea9105dd3171ccc415
jordiprats <jordi.prats@gmail.com>
2019-04-23 22:33:55 +0200

    lint

--- manifests/logindefs/config.pp
+++ manifests/logindefs/config.pp
@@ -1,5 +1,4 @@
 class shadow::logindefs::config inherits shadow::logindefs {
-
   file { '/etc/login.defs':
     ensure  => 'present',
     group   => 'root',
@@ -7,5 +6,4 @@ class shadow::logindefs::config inherits shadow::logindefs {
     mode    => '0644',
     content => template("${module_name}/logindefs/logindefs.erb"),
   }
-
 }
